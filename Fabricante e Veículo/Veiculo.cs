59a87ea52e53f42fd02fd04e23c3dc933998d3bf
Gabriel Cortez <96384061+05041994@users.noreply.github.com>
2022-12-15 10:32:35 -0300

    Update Veiculo.cs

--- Fabricante e Veículo/Veiculo.cs
+++ Fabricante e Veículo/Veiculo.cs
@@ -1,40 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Fabricante_e_Veículo
 {
-    class Veiculo
-    {
-        private string nome, modelo;
-        private double preco;
-
-        public veiculos(string n, string m, double p)
-        {
-            nome = n;
-            modelo = m;
-            if (p > 0) preco = p;
-        }
-        public double Getpreço()
-        {
-            return preco;
-        }
-
-        public override string ToString()
-        {
-            return $"{nome} Veículo do modelo {modelo} Valor: {preco:0.00}";
-        }
-    }
     class Fabricante
     {
         private string nome, sigla;
         private int i = 0;
         private readonly Veiculo[] veiculos = new Veiculo[1];
 
-        public void inserir(Veiculo v)
+        public Fabricante(string n, string s)
+        {
+            nome = n;
+            sigla = s;
+        }
+
+        public void Inserir(Veiculo v)
         {
             if (i == veiculos.Length)
-                Array.Resize(ref veiculos, veiculos.Length + 1);
+            {
+                Array.Resize(ref veiculos, veiculos.Length * 2);
+            }
+
             veiculos[i] = v;
             i++;
         }
@@ -47,15 +35,43 @@ namespace Fabricante_e_Veículo
         public double Maiorpreco()
         {
             Veiculo aux = veiculos[0];
-            for(int k = 1; k < i; k++)
+            for (int k = 1; k < i; k++)
             {
-                if (veiculos[k].Getpreço() > aux.Getpreço())
-                aux = veiculos[k];
+                //if (veiculos[k].Getpreço() > aux.Getpreço())
+                //{
+                //    aux = veiculos[k];
+                //}
+
             }
+
         }
         public override string ToString()
         {
             return $"Nome da fabrica {nome} e a sigla {sigla}";
+        }
+    }
+    class Veiculo
+    {
+        private string nome, modelo;
+        private double preco;
+
+        public Veiculo(string n, string m, double p)
+        {
+            nome = n;
+            modelo = m;
+            if (p > 0)
+            {
+                preco = p;
+            }
+        }
+        public double Getpreço()
+        {
+            return preco;
+        }
+
+        public override string ToString()
+        {
+            return $"{nome} Veículo do modelo {modelo} Valor: {preco:0.00}";
         }
     }
 }
